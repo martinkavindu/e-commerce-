bae8361be687d3766d6a9ed52e9eda583c08dafd
mutuku1998 <martinmutuku1998@gmail.com>
2023-04-14 20:31:47 +0300

    updates

--- al-madina/src/components/Addproduct.js
+++ al-madina/src/components/Addproduct.js
@@ -1,5 +1,24 @@
 import React from "react";
+import {useState} from "react"
+import{storage,db} from "../config/config";
 const Addproduct =()=>{
+    const[ProductName,setProductName] = useState("");
+    const[ProductPrice,setProductPrice]= useState(0);
+    const[ProductImg,setProductImg]= useState(null);
+    const[error,setError]= useState("");
+    const types=['image/png','image/jpeg']
+const ProductImghandler = (e) =>{
+    let selectedFile = e.target.files[0];
+    if(selectedFile && types.includes(selectedFile.type)){
+        setProductImg(selectedFile);
+        setError('');
+    }
+    else{
+        setProductImg(null);
+        setError("please select image type png or jpeg")
+    }
+}
+
     return(
         <div className="container">
             <br/>
@@ -8,17 +27,18 @@ const Addproduct =()=>{
 <form autoComplete="off" className="form-group">
     <label htmlFor="product-name">Product Name</label>
     <br/>
-<input type="text" className="form-control"required/>
+<input type="text" className="form-control"required  onChange={(e)=>setProductName(e.target.value)} value={ProductName}/>
 <br/>
 <label htmlFor=" product-price">Product Price</label>
 <br/>
-<input type="number"className="form-control"required/>
+<input type="number"className="form-control"required  onChange={(e)=>setProductPrice(e.target.value)} value={ProductPrice}/>
 <br/>
 <label htmlFor="product-img">Product Image</label>
-<input  type="file" className="form-control"/>
+<input  type="file" className="form-control" onChange={ProductImghandler}/>
 <br/>
 <button className="btn btn-success btn-md mybtn">ADD</button>
     </form>
+    {error && <span>{error}</span>}
 
         </div>
     )
--- al-madina/src/config/config.js
+++ al-madina/src/config/config.js
@@ -2,15 +2,15 @@ import * as firebase from 'firebase';
 import "firebase/auth";
 import "firebase/firestore";
 import"firebase/storage";
+import { initializeApp } from "firebase/app";
 const firebaseConfig = {
-    apiKey: "AIzaSyC_OeLbnYEn6zqiVy-7ilqHihSsepTMlys",
-    authDomain: "almedina-hardware.firebaseapp.com",
-    projectId: "almedina-hardware",
-    storageBucket: "almedina-hardware.appspot.com",
-    messagingSenderId: "967410439718",
-    appId: "1:967410439718:web:8b3e5f90e906be4d31b8e7"
-  };
-  const app = initializeApp(firebaseConfig);
+  apiKey: "AIzaSyC_OeLbnYEn6zqiVy-7ilqHihSsepTMlys",
+  authDomain: "almedina-hardware.firebaseapp.com",
+  projectId: "almedina-hardware",
+  storageBucket: "almedina-hardware.appspot.com",
+  messagingSenderId: "967410439718",
+  appId: "1:967410439718:web:8b3e5f90e906be4d31b8e7"
+};  const app = initializeApp(firebaseConfig);
   const auth = firebase.auth();
   const db = firebase.firestore();
   const storage = firebase.storage();
